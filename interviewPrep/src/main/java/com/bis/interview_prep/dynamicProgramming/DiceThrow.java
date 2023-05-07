1b53a590df4db327c397d9f6cab21b34d5fa9791
nyame <bismark.nyame@ecomtrading.com>
2023-05-07 20:26:56 +0000

    Correcting the code for the dice throw problem

--- interviewPrep/src/main/java/com/bis/interview_prep/dynamicProgramming/DiceThrow.java
+++ interviewPrep/src/main/java/com/bis/interview_prep/dynamicProgramming/DiceThrow.java
@@ -12,9 +12,40 @@ import java.util.Map;
 public class DiceThrow {
 
     public static void main(String[] args) {
-        int face = 6, numberOfTimes = 3, total = 7;
-        int numWays = findWaysTopDown(face, numberOfTimes, total);
+        int face = 6, numberOfTimes = 3, total = 9;
+        long numWays = diceThrowBottomUp(face, numberOfTimes, total);
         System.out.println(numWays);
+    }
+
+    public static long findWays(int m, int n, int x) {
+
+    /* Create a table to store the results of subproblems.
+    One extra row and column are used for simplicity
+    (Number of dice is directly used as row index and sum is directly used as column index).
+    The entries in 0th row and 0th column are never used. */
+        long[][] table = new long[n + 1][x + 1];
+
+        /* Table entries for only one dice */
+        for (int j = 1; j <= m && j <= x; j++)
+            table[1][j] = 1;
+
+    /* Fill rest of the entries in table using recursive relation
+    i: number of dice, j: sum */
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= x; j++) {
+                for (int k = 1; k < j && k <= m; k++)
+                    table[i][j] += table[i - 1][j - k];
+            }
+        }
+
+         // Uncomment these lines to see content of table
+        for(int i = 0; i< n+1; i++){
+            for(int j = 0; j< x+1; j++)
+                System.out.print(table[i][j] + " ");
+            System.out.println();
+        }
+
+        return table[n][x];
     }
 
     private static int diceThrowBottomUp(int face, int numberOfTimes, int total) {
@@ -27,10 +58,18 @@ public class DiceThrow {
             for (int j = i; j <= total; j++) {
                 dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1];
                 if (j - face - 1 >= 0) {
-                    dp[i][j] -= dp[i][j - face - 1];
+                    dp[i][j] -= dp[i-1][j - face - 1];
                 }
             }
         }
+
+        // Uncomment these lines to see content of table
+        for(int i = 0; i< numberOfTimes+1; i++){
+            for(int j = 0; j< total+1; j++)
+                System.out.print(dp[i][j] + " ");
+            System.out.println();
+        }
+
 
         return dp[numberOfTimes][total];
     }
@@ -52,7 +91,7 @@ public class DiceThrow {
         for (i = 1; i <= faces; ++i)
             numberOfWays += findWays(faces, numTimes - 1, total - i, memo);
 
-        memo.put(key,numberOfWays);
+        memo.put(key, numberOfWays);
         return numberOfWays;
     }
 }
